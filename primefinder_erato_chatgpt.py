256b648e528c994c2808fe826f51c8d08c148fcb
Pjotr7575 <pjotr975@gmail.com>
2023-09-29 22:20:17 +0200

    I wrote some comment, and commit some new file from Pycharm

--- primefinder_erato_chatgpt.py
+++ primefinder_erato_chatgpt.py
@@ -1,3 +1,8 @@
+# Prímszám kereső Eratosztenész szitája alapján
+# a kód alapja chatGPT-től származik
+# chatGPT link: https://chat.openai.com/share/178e526e-d0bd-48b9-8e1a-8f47e035ed32
+# ez a változat hibásan működik, mivel minden tartományban 2-től keresi a prímeket
+
 def eratosthenes_sieve(n):
     sieve = [True] * (n + 1)
     p = 2
@@ -8,6 +13,8 @@ def eratosthenes_sieve(n):
         p += 1
     primes = [p for p in range(2, n + 1) if sieve[p]]
     return primes
+
+
 n2 = int(input("Add meg a felső limitet  (legyen 1000-el osztható)! "))
 # Első tartomány: 2-től 1000-ig
 n1 = int(n2 / 2)
@@ -15,7 +22,7 @@ primes1 = eratosthenes_sieve(n1)
 
 # Második tartomány: 1001-től 2000-ig
 primes2 = eratosthenes_sieve(n2)
-primes2 = [p for p in primes2 if p >= n1+1]  # Szűrjük csak az 1001 és 2000 közötti prímekre
+primes2 = [p for p in primes2 if p >= n1 + 1]  # Szűrjük csak az 1001 és 2000 közötti prímekre
 
 # print(primes1)
 # print()
@@ -25,4 +32,3 @@ print("primes1 lenght: ", len(primes1))
 print("primes2 lenght: ", len(primes2))
 print("sum lenght: ", (len(primes1) + len(primes2)))
 print("last prime: ", primes2[-1])
-
